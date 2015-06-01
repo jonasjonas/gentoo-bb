f3065bd9316a2b7104d25bd9016073e99c390fc0
Frank Hellenkamp <jonas@depage.net>
2015-06-01 19:52:50 +0200

    added new script to start nginx and mariadb

---
+++ bin/nginx_php_mariadb.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+VHOST_BASE=test.void
+ADMINER_URL=db.${VHOST_BASE}
+PHPINFO_URL=phpinfo.${VHOST_BASE}
+
+XDEBUG=yes
+XDEBUG_LOCAL_PORT=9000
+
+VHOST_URL=${VHOST_BASE},${ADMINER_URL},${PHPINFO_URL}
+
+USER_UID=$(id -u $(whoami))
+USER_GID=$(id -g $(whoami))
+
+start () {
+    LOCAL_WEB=${1}
+    LOCAL_DB=${2}
+
+    # mariadb
+    docker run -d \
+        --name www_mariadb \
+        --hostname www_mariadb \
+        -v ${LOCAL_DB}:/var/lib/mysql/ \
+        gentoobb/mariadb
+
+    MYSQL_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' www_mariadb`
+
+    # nginx php
+    docker run -d \
+        -e VIRTUAL_HOST="$VHOST_URL" \
+        -e NG_TMPL_ADMINER_URL="$ADMINER_URL" \
+        -e NG_TMPL_PHPINFO_URL="$PHPINFO_URL" \
+        -e NGINX_UID="$USER_UID" \
+        -e NGINX_GID="$USER_GID" \
+        -e XDEBUG_ENABLED="$XDEBUG" \
+        -p $XDEBUG_LOCAL_PORT:9000 \
+        -p 80:80 \
+        -p 443:443 \
+        --link www_mariadb:db \
+        --name www_php \
+        --hostname www_php \
+        -v ${LOCAL_WEB}:/var/www/localhost \
+        gentoobb/nginx-php5.5
+
+    NGINX_IP=`docker inspect --format '{{ .NetworkSettings.IPAddress }}' www_php`
+
+    echo -e "\nmysql:\t\t $MYSQL_IP:3306 login: root/root"
+    echo -e "nginx:\t\t http://$NGINX_IP/"
+    if [[ "$XDEBUG" == 'yes' ]]; then
+        echo -e "xdebug enabled:\t yes, mapped to host port: $XDEBUG_LOCAL_PORT"
+    else
+        echo -e "xdebug:\t\t no"
+    fi
+    echo -e "vhost:\t\t http://$VHOST_BASE/"
+    echo -e "adminer:\t http://$ADMINER_URL/adminer.php?server=db"
+    echo -e "phpinfo:\t http://$PHPINFO_URL/"
+}
+
+stop () {
+    echo "stopping container:"
+    docker stop www_mariadb
+    docker stop www_php
+    echo "destroying container:"
+    docker rm www_mariadb
+    docker rm www_php
+}
+
+case "${1}" in
+    start) start ${2} $3;;
+    stop) stop;;
+*) echo  "
+Start or stop a simple nginx php5.5 mariadb webstack.
+
+usage: ${0} start|stop [local_web_dir] [local_mysql_dir]";;
+esac
